5144411b5af2a70b8034b593e23a61d5e709568b
SadGod <github@svdgod.ru>
2024-05-28 16:56:24 +0500

    delete beatmaps by number objects and time

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
 module.exports = {
 	osu_path: 'D:\\osu!',
 	backup_folder: 'Songs_backup',
+
+	conf_delete_beatmaps: {
+		min_number_objects: 30,
+		min_drain_time: 30,
+		min_total_time: 30,
+		backup_instead_delete: false
+	}
 }
--- tools.js
+++ tools.js
@@ -17,7 +17,42 @@ const _this =module.exports = {
 				_this.check_folder(path.join(dest_path, x.folder));
 				fs.renameSync(source_filepath, dest_filepath);
 			} catch (e) {
-				console.error(e)
+				console.error(e);
+			}
+		}
+	},
+
+	move_to_2: (beatmaps, source_path, dest_path) => {
+		for (x of beatmaps) {
+			const source_filepath = path.join(source_path, x.folder_name, x.osu_filename);
+			const dest_filepath = path.join(dest_path, x.folder_name, x.osu_filename);
+			try {
+				_this.check_folder(path.join(source_path, x.folder_name));
+				_this.check_folder(path.join(dest_path, x.folder_name));
+				if(fs.existsSync(source_filepath)) {
+					fs.renameSync(source_filepath, dest_filepath);
+					console.log(`backup: ${source_filepath} to ${dest_filepath}`);
+				} else {
+					console.log(`backup: ${source_filepath} not found`);
+				}
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	},
+
+	delete_from: (beatmaps, source_path) => {
+		for (x of beatmaps) {
+			const source_filepath = path.join(source_path, x.folder_name, x.osu_filename);
+			try {
+				if(fs.existsSync(source_filepath)) {
+					fs.unlinkSync(source_filepath);
+					console.log(`delete: ${source_filepath}`);
+				} else {
+					console.log(`delete: ${source_filepath} not found`);
+				}
+			} catch (e) {
+				console.error(e);
 			}
 		}
 	},
